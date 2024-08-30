fca91883ac5e65efb438c705124a2a8f62474cc8
Henryekklesia <ekklesiamedialive@gmail.com>
2024-08-30 11:36:40 +0100

    Betty checks complete

--- Betty_checks.c
+++ Betty_checks.c
@@ -1,8 +1,13 @@
 #include <stdio.h>
+/**
+ * Description - Simple COde that pass betty checks
+ * main - main function
+ * Return: Always (0) sucess
+ */
 
 int main(void)
 {
+	printf("Betty will be happy!");
 
-	function;
 	return (0);
 }
